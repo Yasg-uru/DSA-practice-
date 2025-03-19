afe525b3808b8977b3a7d30c09e7ab4b0c1562ce
yasg-uru <yashpawar12122004@gmail.com>
2025-03-19 10:55:53 +0530

    added leetcode 3191

---
+++ Bit_Manupulation/medium/3191. Minimum Operations to Make Binary Array Elements Equal to One I.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+class Solution {
+    public:
+        int minOperations(vector<int>& nums) {
+            int n = nums.size();
+            int operations = 0;
+            for(int i=0;i<n-2;i++){
+                if(nums[i]==1){
+                    continue;
+                }
+                operations++;
+                int j = i ;
+                while(j<i+3){
+                    nums[j]= nums[j]^1;
+                    j++;
+    
+                }
+    
+            }
+            if(nums[n-1]==0 or nums[n-2]==0){
+                return -1 ;
+            }
+            return operations ;
+        }
+    };
